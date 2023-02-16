2c2c89cba0e3e2787c0893223ebdb1ffa211989c
Michael <panmpan@gmail.com>
2023-02-16 20:58:45 +0800

    Initial Commit

---
+++ CPlusPlus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPlusPlus
+//
+//  Created by Michael Pan on 2023/2/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
